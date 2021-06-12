109e79e204bd19257f9f633e091052c05610e42c
Juan carlos salcedo <juank9225@gmail.com>
2021-06-11 23:06:31 -0500

    Se crean los metodos para testiar la division, un caso feliz y un caso triste evaluando el cero

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -1,5 +1,6 @@
 package co.com.sofka.app.calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -54,5 +55,31 @@ class BasicCalculatorTest {
     public void severalmulplication(Long first, Long second, Long expectedResult){
         assertEquals(expectedResult,cal.mulplication(first,second),
                 ()->first+" "+second+"should equal "+expectedResult);
+    }
+
+    @DisplayName("Testing several Subtracting")
+    @ParameterizedTest(name = "{5}/{5}={1}")
+    @CsvSource({
+            "25, 5, 5",
+            "14, 2, 7",
+            "8, 4, 2",
+            "100, 50, 2"
+    })
+    public void serveraldivision(Long first, Long second, Long expectedResult){
+        assertEquals(expectedResult,cal.division(first,second),
+                ()->first+" "+second+" should equal "+expectedResult);
+    }
+
+    @DisplayName("Testing several Subtracting")
+    @ParameterizedTest(name = "{5}/{0}")
+    @CsvSource({
+            "5, 0,",
+            "0, 0"
+    })
+    public void sadcasedivision(Long first, Long second){
+     var response=   Assertions.assertThrows(RuntimeException.class,
+                ()->{Long result = cal.division(first,second);
+        });
+        assertEquals("No se puede dividir por cero",response.getMessage());
     }
 }
