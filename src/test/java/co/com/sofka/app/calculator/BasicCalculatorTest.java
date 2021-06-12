8f169d06bb9d88d15ac00065d833cfb9b555f0d4
Juan carlos salcedo <juank9225@gmail.com>
2021-06-11 19:01:11 -0500

    Se crean las pruebas para los metodos de restar y multiplicar dos numeros

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -29,4 +29,30 @@ class BasicCalculatorTest {
         assertEquals(expectedResult, cal.sum(first,second),
                 ()->first+" "+second+" should equal "+ expectedResult);
     }
+
+    @DisplayName("Testing several Subtracting")
+    @ParameterizedTest(name = "{1}-{2}={1}")
+    @CsvSource({
+            "0, 1, -1",
+            "2, 1, 1",
+            "50, 20, 30",
+            "40, 30, 10"
+    })
+    public void severalSubtracting(Long first, Long second, Long expectedResult){
+        assertEquals(expectedResult,cal.substraction(first,second),
+                ()->first+" "+second+"should equal "+expectedResult);
+    }
+
+    @DisplayName("Testing several Subtracting")
+    @ParameterizedTest(name = "{1}x{2}={1}")
+    @CsvSource({
+            "0, 1, 0",
+            "2, 1, 2",
+            "50, 20, 1000",
+            "40, 30, 1200"
+    })
+    public void severalmulplication(Long first, Long second, Long expectedResult){
+        assertEquals(expectedResult,cal.mulplication(first,second),
+                ()->first+" "+second+"should equal "+expectedResult);
+    }
 }
